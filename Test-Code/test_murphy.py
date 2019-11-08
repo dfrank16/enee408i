fb89a6d419d90b03a006f9c0730283847f8e1f6b
Dillon Frank <dfrank16@eng.umd.edu>
2019-11-08 12:43:48 -0500

    Added music functionality

--- Test-Code/test_murphy.py
+++ Test-Code/test_murphy.py
@@ -1,13 +1,32 @@
 from flask import Flask, render_template
-from flask_ask import Ask, statement, question
+from flask_ask import Ask, statement, question, audio, current_stream, logger
 import time
 import serial
 import struct
+import collections
+from copy import copy
+import logging
+
 
 ser = serial.Serial('/dev/ttyACM0',9600)
 
 app = Flask(__name__)
 ask = Ask(app, '/')
+logging.getLogger('flask_ask').setLevel(logging.INFO)
+
+playlist = [
+    # 'https://www.freesound.org/data/previews/367/367142_2188-lq.mp3',
+    'https://archive.org/download/mailboxbadgerdrumsamplesvolume2/Ringing.mp3',
+    'https://archive.org/download/petescott20160927/20160927%20RC300-53-127.0bpm.mp3',
+    'https://archive.org/download/plpl011/plpl011_05-johnny_ripper-rain.mp3',
+    'https://archive.org/download/piano_by_maxmsp/beats107.mp3',
+    'https://archive.org/download/petescott20160927/20160927%20RC300-58-115.1bpm.mp3',
+    'https://archive.org/download/PianoScale/PianoScale.mp3',
+    # 'https://archive.org/download/FemaleVoiceSample/Female_VoiceTalent_demo.mp4',
+    'https://archive.org/download/mailboxbadgerdrumsamplesvolume2/Risset%20Drum%201.mp3',
+    'https://archive.org/download/mailboxbadgerdrumsamplesvolume2/Submarine.mp3',
+    # 'https://ia800203.us.archive.org/27/items/CarelessWhisper_435/CarelessWhisper.ogg'
+]
 
 
 def halt():
@@ -34,12 +53,112 @@ def wander():
 	valueToWrite= 5
 	ser.write(struct.pack('>B', valueToWrite))
 
+class QueueManager(object):
+    """Manages queue data in a seperate context from current_stream.
+    The flask-ask Local current_stream refers only to the current data from Alexa requests and Skill Responses.
+    Alexa Skills Kit does not provide enqueued or stream-histroy data and does not provide a session attribute
+    when delivering AudioPlayer Requests.
+    This class is used to maintain accurate control of multiple streams,
+    so that the user may send Intents to move throughout a queue.
+    """
 
+    def __init__(self, urls):
+        self._urls = urls
+        self._queued = collections.deque(urls)
+        self._history = collections.deque()
+        self._current = None
+
+    @property
+    def status(self):
+        status = {
+            'Current Position': self.current_position,
+            'Current URL': self.current,
+            'Next URL': self.up_next,
+            'Previous': self.previous,
+            'History': list(self.history)
+        }
+        return status
+
+    @property
+    def up_next(self):
+        """Returns the url at the front of the queue"""
+        qcopy = copy(self._queued)
+        try:
+            return qcopy.popleft()
+        except IndexError:
+            return None
+
+    @property
+    def current(self):
+        return self._current
+
+    @current.setter
+    def current(self, url):
+        self._save_to_history()
+        self._current = url
+
+    @property
+    def history(self):
+        return self._history
+
+    @property
+    def previous(self):
+        history = copy(self.history)
+        try:
+            return history.pop()
+        except IndexError:
+            return None
+
+    def add(self, url):
+        self._urls.append(url)
+        self._queued.append(url)
+
+    def extend(self, urls):
+        self._urls.extend(urls)
+        self._queued.extend(urls)
+
+    def _save_to_history(self):
+        if self._current:
+            self._history.append(self._current)
+
+    def end_current(self):
+        self._save_to_history()
+        self._current = None
+
+    def step(self):
+        self.end_current()
+        self._current = self._queued.popleft()
+        return self._current
+
+    def step_back(self):
+        self._queued.appendleft(self._current)
+        self._current = self._history.pop()
+        return self._current
+
+    def reset(self):
+        self._queued = collections.deque(self._urls)
+        self._history = []
+
+    def start(self):
+        self.__init__(self._urls)
+        return self.step()
+
+    @property
+    def current_position(self):
+        return len(self._history) + 1
+
+queue = QueueManager(playlist)
 
 @ask.launch
 def launched():
     return question("Hello. what would you like Murphy to do?").reprompt(
         "if you don't need Murphy, please tell him to go to sleep.")
+
+@ask.intent('PlayMusicIntent')
+def playMusic():
+    speech = 'Heres a playlist of some sounds. You can ask me Next, Previous, or Start Over'
+    stream_url = queue.start()
+    return audio(speech).play(stream_url)
 
 
 @ask.intent('ForwardIntent')
