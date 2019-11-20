dee87a5cb3f6caab93bb8b5defc5b07e7923c738
Jack <jack.a.sturtevant@gmail.com>
2019-11-20 13:52:16 -0500

    MURPHY'S MOVIN' BOIS

--- Test-Code/test_murphy.py
+++ Test-Code/test_murphy.py
@@ -14,10 +14,15 @@ import sys
 import errno
 import threading
 import math
+from math import atan2, asin
+from imutils.video import VideoStream
+import apriltag
+import cv2
+import imutils
 
 
 ser = serial.Serial('/dev/ttyACM0',9600)
-
+ser.timeout = 1.0
 waiting = True
 app = Flask(__name__)
 ask = Ask(app, '/')
@@ -29,7 +34,7 @@ logging.getLogger('flask_ask').setLevel(logging.INFO)
 # python ball_tracking.py
 
 # import the necessary packages
-
+followFlag = 0
 state = 0
 goal_x = 0.0
 goal_z = 0.0
@@ -64,29 +69,84 @@ username_header = f"{len(username):<{HEADER_LENGTH}}".encode('utf-8')
 print("done with socket")
 
 
+camera_matrix = [[1.78083891e+03, 0.00000000e+00, 9.35628820e+02], [0.00000000e+00, 2.15671011e+03, 5.38832732e+02],[0.00000000e+00, 0.00000000e+00, 1.00000000e+00]]
+
+camera_distortions = [[ 1.41784728e+00, -5.29012388e+01, 4.59024886e-04, 3.03192456e-02,4.97251723e+02]]
+
+camera_distortions = numpy.array(camera_distortions)
+camera_matrix = numpy.array(camera_matrix)
+
+
+# Load world points
+world_points = {}
+with open('worldPoints.json', 'r') as f:
+        data = json.load(f)
+for k,v in data.items():
+        world_points[int(k)] = numpy.array(v, dtype=numpy.float32).reshape((4,3,1))
+
+def get_orientation(camera_matrix, R, t):
+        proj = camera_matrix.dot(numpy.hstack((R, t)))
+        rot = cv2.decomposeProjectionMatrix(proj)
+        rot = rot[-1]
+        return rot[1], rot[2], rot[0]
+
+
+def arduino_write_fail():
+        print("Serial write to arduino timed out. Resetting connection")
+        ser.close()
+        ser.open()
+drive_state = 0
 def halt():
-	valueToWrite= 0
-	ser.write(struct.pack('>B', valueToWrite))
+        global drive_state
+        if drive_state != 0:
+                try:
+                        ser.write(struct.pack('>B', 0))
+                        drive_state = 0
+                except:
+                        arduino_write_fail()
 
 def left():
-	valueToWrite= 1
-	ser.write(struct.pack('>B', valueToWrite))
-
+        global drive_state
+        if drive_state != 1:
+                try:
+                        ser.write(struct.pack('>B', 1))
+                        drive_state = 1
+                except:
+                        arduino_write_fail()
 def right():
-	valueToWrite= 2
-	ser.write(struct.pack('>B', valueToWrite))
-
+        global drive_state
+        if drive_state != 2:
+                try:
+                        ser.write(struct.pack('>B', 2))
+                        drive_state = 2
+                except:
+                        arduino_write_fail()
 def forward():
-	valueToWrite= 3
-	ser.write(struct.pack('>B', valueToWrite))
-
+        global drive_state
+        if drive_state != 3:
+                try:
+                        ser.write(struct.pack('>B', 3))
+                        drive_state = 3
+                except:
+                        arduino_write_fail()
 def backward():
-	valueToWrite= 4
-	ser.write(struct.pack('>B', valueToWrite))
-	
+        global drive_state
+        if drive_state != 4:
+                try:
+                        ser.write(struct.pack('>B', 4))
+                        drive_state = 4
+                except:
+                        arduino_write_fail()
 def wander():
-	valueToWrite= 5
-	ser.write(struct.pack('>B', valueToWrite))
+        global drive_state
+        if drive_state != 5:
+                try:
+                        ser.write(struct.pack('>B', 5))
+                        drive_state = 5
+                except:
+                        arduino_write_fail()
+
+
 
 def start_app():
     app.run(debug=True)
@@ -232,6 +292,18 @@ def wander_command(command):
 def attack():
     return question("Perkele!").reprompt("Murphy has calmed down now. What would you like him to do?")
 
+@ask.intent('FollowMeIntent')
+def followMeHandler():
+	global followFlag
+	followFlag = 1
+	thread.start_new_thread(followPerson, ())
+
+@ask.intent('StayIntent')
+def stayHandler():
+	global followFlag
+	followFlag = 0
+	halt()
+	
 
 @ask.intent('RollIntent')
 def rollOver():    
@@ -392,7 +464,37 @@ def findDistress():
     halt()
     return
 
+def followPerson():
+	global followFlag
+	vs = VideoStream(src=1).start()
+	time.sleep(2.0)
+	detector = apriltag.Detector()
 
+	# keep looping
+	while followFlag:
+		time.sleep(.1)
+		frame = vs.read()
+		frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		atags = detector.detect(frame)
+		temp_origin = numpy.matrix([[0, 0, 0], [1, 0, 0], [1, -1, 0], [0, -1, 0]])
+		for tag in atags:
+			corners = tag.corners
+			corners = numpy.array(corners, dtype=numpy.float32).reshape((4,2,1))
+			tag_id = tag.tag_id
+			if tag.tag_id == 50:
+				center = tag.center
+				x = center[0]
+				if  pose[2] > 5.0:
+					if x<150:
+						left()
+					elif x>410:
+						right()
+					elif x>=150 and x <= 410:
+						forward()
+				else:
+					halt()
+	vs.release()
+	halt()
 
 if __name__ == '__main__':
 	print("starting app")
