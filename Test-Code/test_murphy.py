a0cdc323653ddbae82c96cf8b553e0ede9c42028
Dillon Frank <dfrank16@eng.umd.edu>
2019-12-03 17:14:03 -0500

    Added dance code

--- Test-Code/test_murphy.py
+++ Test-Code/test_murphy.py
@@ -265,6 +265,32 @@ def wander_command(command):
         halt()
         return question("Murphy has stopped wandering.").reprompt("what would you like Murphy to do now?")
 
+@ask.intent('DanceIntent')
+def danceIntent():
+    t = threading.Thread(target=dance, name='t_dance')
+    t.start()
+    return question("To the left, take it back now yall. One hop this time. Right foot let's stomp. Left foot let's stomp. Cha cha real smooth. Doot do doot do.").reprompt("Yo dog, my moves are fresh. What do you want me to do next?")
+
+def dance():
+    left()
+    time.sleep(2.0)
+    backward()
+    time.sleep(2.0)
+    halt()
+    time.sleep(0.5)
+    forward()
+    time.sleep(0.5)
+    halt()
+    time.sleep(2.0)
+    right()
+    time.sleep(0.5)
+    left()
+    time.sleep(0.5)
+    halt()
+    time.sleep(1.0)
+    right()
+    time.sleep(5.0)
+    halt()
 
 @ask.intent('AttackIntent')
 def attack():
