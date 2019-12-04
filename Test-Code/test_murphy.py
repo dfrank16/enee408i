741e5ba46a06c059167a7437a915bfda7e04546e
Dillon Frank <dfrank16@eng.umd.edu>
2019-12-03 19:35:26 -0500

    Added medicine code

--- Test-Code/test_murphy.py
+++ Test-Code/test_murphy.py
@@ -36,6 +36,7 @@ logging.getLogger('flask_ask').setLevel(logging.INFO)
 
 # import the necessary packages
 followFlag = 0
+taken = False
 state = 0
 goal_x = 0.0
 goal_z = 0.0
@@ -50,6 +51,7 @@ x_pos = 90.0
 x_neg = -90.0
 frame = None
 stop = False
+taken = False
 
 HEADER_LENGTH = 10
 IP = "192.168.43.59"
@@ -269,9 +271,9 @@ def wander_command(command):
 def danceIntent():
     t = threading.Thread(target=dance, name='t_dance')
     t.start()
-    danceString = """To the left, take it back now yall. One hop this time <break time="2s">. Right foot let's stomp. 
-                    Left foot let's stomp. Cha cha real smooth. Doot do doot do.
-                     . . . . j . j. j. l . l . l . u . u . u . u . oh oh oh oh oh c c c c c c"""
+    danceString = """To the left, take it back now yall. One hop this time <breaktime="2s"> . Right foot let's,,,,,,,, stomp. 
+                    Left foot let's stomp. Cha cha real smooth. Doot do doot do. Doot do doot do. Doot do doot do. 
+                     """
     return question(danceString).reprompt("Yo dog, my moves are fresh. What do you want me to do next?")
 
 def dance():
@@ -291,12 +293,21 @@ def dance():
     time.sleep(0.5)
     halt()
     time.sleep(1.0)
-    for i in range(0, 10):
+    for i in range(0, 3):
         left()
         time.sleep(0.5)
         right()
         time.sleep(0.5)
     halt()
+
+@ask.intent('MedicineIntent')
+def medicine():
+    global taken
+    if taken:
+        return question("Yes, you have taken your medication already.")
+    else:
+        taken = True
+        return question("No, but you should take your medicine now.")
 
 @ask.intent('AttackIntent')
 def attack():
